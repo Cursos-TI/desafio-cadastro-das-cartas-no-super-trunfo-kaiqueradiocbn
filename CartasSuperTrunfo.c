0d0ff1bf6184142d83607ea85862be420082e074
kaiqueradiocbn <kaique.radiocbn@gmail.com>
2025-07-31 17:09:23 +0000

    formatação de cadastro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,24 +13,50 @@ int main() {
         int pontos1, pontos2;   
         
     //Cadastro Carta 1
+
+    
     printf ("Carta 1:\n");
+
+    printf("Digite o código da Carta: \n");
     scanf ("%s", &código1);
+
+    printf("Digite a população: \n");
     scanf ("%d", &populacao1);
+
+    printf ("Digite a área em KM: \n");
     scanf ("%f", &área1);
+
+    printf("Digite a população em Milhões: \n");
     scanf ("%f", &pib1);
+
+    printf ("Digite quantos pontos turísticos: \n");
     scanf ("%d", &pontos1);
 
+
+
         //Cadastro Carta 2
-    printf ("Carta 1:\n");
+
+
+   
+    printf ("Carta 2:\n");
+
+    printf("Digite o código da Carta: \n");
     scanf ("%s", &código2);
+
+    printf("Digite a população: \n");
     scanf ("%d", &populacao2);
+
+    printf ("Digite a área em KM: \n");
     scanf ("%f", &área2);
+
+    printf("Digite a população em Milhões: \n");
     scanf ("%f", &pib2);
+
+    printf ("Digite quantos pontos turísticos: \n");
     scanf ("%d", &pontos2);
 
-
-
-    //Dados de saída
+     //Dados de saída
+     printf("Seu código é:")
 
     printf("")
 

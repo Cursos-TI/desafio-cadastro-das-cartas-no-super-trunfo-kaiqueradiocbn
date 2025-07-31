87e49f6dfcce08a258c4d897cab37479de0cf061
kaiqueradiocbn <kaique.radiocbn@gmail.com>
2025-07-31 16:38:16 +0000

    cadastro das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,11 +6,56 @@ int main() {
     
     // Valores variasveis Carta 1 e Carta 2
 
-        int População1, População2;
+        char código1[4], código2[4]
+        int Populacao1, Populacao2;
         float Área1, Área2;
         float Pib1, Pib2;
-        int Pontos1, Pontos2;      
- 
+        int Pontos1, Pontos2;   
+        
+    //Cadastro Carta 1
+    printf ("Carta 1:\n");
+
+    printf ("Código da Carta:");
+    scanf ("%s", &código1);
+
+    printf (" Sua População é de:");
+    scanf ("%f", &Populacao1:);
+
+    printf ("Sua Área é de:");
+    scanf ("%f", &Área1);
+
+    printf ("Pib:");
+    scraf ("%f", &Pib1);
+
+    printf (" Pontos Turisticos:");
+    scanf ("%d" &Pontos1)
+
+
+
+        //Cadastro Carta 2
+    printf ("Carta 2:\n");
+
+    printf ("Código da Carta:");
+    scanf ("%s", &código2);
+
+    printf (" Sua População é de:");
+    scanf ("%f", &Populacao2:);
+
+    printf ("Sua Área é de:");
+    scanf ("%f", &Área2);
+
+    printf ("Pib:");
+    scraf ("%f", &Pib2);
+
+    printf (" Pontos Turisticos:");
+    scanf ("%d" &Pontos2)
+
+
+
+
+
+    
 
     return 0;
 }
+ 

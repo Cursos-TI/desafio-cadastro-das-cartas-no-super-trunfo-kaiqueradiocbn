1355036756552d2074511e6a6685a448f56d7906
kaiqueradiocbn <kaique.radiocbn@gmail.com>
2025-07-31 16:06:12 +0000

    cadastro de variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,9 +2,14 @@
 
 
 int main() {
+    printf("Super Trunfo\n");
+    
     // Valores variasveis Carta 1 e Carta 2
 
         int População1, População2;
+        float Área1, Área2;
+        float Pib1, Pib2;
+        int Pontos1, Pontos2;      
  
 
     return 0;
